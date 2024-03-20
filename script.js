61a7c92650581c06a9a5f2a0e90e4528ceb99985
Ciloe <gabiciloe@gmail.com>
2024-03-20 09:45:31 -0300

    Hamburgueria concluida
    Hamburgueria concluida

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const cartTotal = document.getElementById('cart-total');
 const checkoutBtn = document.getElementById('checkout-btn');
 const closeModalBtn = document.getElementById('close-modal-btn');
 const cartCounter = document.getElementById('cart-count');
-const addresInput = document.getElementById('address');
-const adressWarn = document.getElementById('address-warn');
+const addressInput = document.getElementById('address');
+const addressWarn = document.getElementById('address-warn');
 
 let cart = [];
 
@@ -85,7 +85,7 @@ function updateCartModal() {
             </div>
 
             
-                <button>
+                <button class="remove-from-cart-btn" data-name="${item.name}">
                     Remover
                 </button>
             
@@ -101,4 +101,101 @@ function updateCartModal() {
         style: "currency",
         currency: "BRL"
     });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+
+//Função para remover o item do carrinho
+cartItemsContainer.addEventListener('click', function(event){
+    if(event.target.classList.contains('remove-from-cart-btn')){
+        const name = event.target.getAttribute('data-name');
+        
+        removeItemCart(name);
+    }
+
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+addressInput.addEventListener('input', function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove('border-red-500');
+        addressWarn.classList.add('hidden');
+        
+    }
+})
+
+
+//Finalizar pedido
+checkoutBtn.addEventListener('click', function(){
+    
+    
+    
+    const isOpen = checkRestauranteOpen();
+    if(!isOpen){
+        alert("RESTAURANTE FECHADO NO MOMENTO!");
+        return;
+    }
+    
+
+    if(cart.length === 0)return;
+
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden");
+        addressInput.classList.add('border-red-500');
+        return;
+    }
+
+
+    //Enviar o pedido para API WhatsApp
+
+    const cartItems = cart.map((item) => {
+        return(
+            `Lanche: ${item.name}, Quantidade: (${item.quantity}) Preço: R$${item.price} | `
+        )
+    }).join("");
+
+    const message = encodeURIComponent(cartItems);
+    const phone = "19997939514";
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+})
+
+
+//Verificar a hora e manipular o card do horário
+function checkRestauranteOpen(){
+    const data  =  new Date();
+    const hora  = data.getHours();
+    return hora >= 18 && hora < 23; 
+    //true = restaurante aberto
+}
+
+const spanItem = document.getElementById('date-span');
+const isOpen = checkRestauranteOpen();
+
+if(isOpen){
+    spanItem.classList.remove('bg-red-500');
+    spanItem.classList.add('bg-green-600');
+}else{
+    spanItem.classList.remove('bg-green-600');
+    spanItem.classList.add('bg-red-500');
 }
